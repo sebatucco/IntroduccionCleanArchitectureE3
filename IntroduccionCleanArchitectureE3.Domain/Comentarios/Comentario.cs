b59ba973e2842c8217999e2aa83232c6477505e4
sebatucco <c.sebastian.ojeda@gmail.com>
2024-04-11 17:12:08 -0300

    ultimos cambios

---
+++ IntroduccionCleanArchitectureE3.Domain/Comentarios/Comentario.cs
@@ -0,0 +1,46 @@
+﻿using IntroduccionCleanArchitectureE3.Domain.Abstractions;
+using IntroduccionCleanArchitectureE3.Domain.Alquileres;
+using IntroduccionCleanArchitectureE3.Domain.Alquileres.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionCleanArchitectureE3.Domain.Comentarios
+{
+    public sealed class Comentario : Entity
+    {
+        public Guid VehiculoId { get; private set; }
+        public Guid AlquilerId { get; private set; }
+        public Guid UserId { get; private set; }
+        public int Rating { get; private set; }
+        public string? Review { get; private set; } 
+        public DateTime? FechaCreacion { get; private set; } 
+        private Comentario(
+            Guid id,
+            Guid vehiculoId,
+            Guid alquilerId,
+            Guid userId,
+            int rating,
+            string review,
+            DateTime? fechaCreacion
+            ) : base(id)
+        {
+            VehiculoId = vehiculoId;
+            AlquilerId = alquilerId;
+            UserId = userId;
+            Rating = rating;
+            Review = review;
+            FechaCreacion = fechaCreacion;      
+        }
+
+        public static Result<Comentario> Create(Alquiler alquiler, int rating, string comentario, DateTime fechacreacion)
+        {
+            if (alquiler.Status != AlquilerStatus.Completado)
+            { 
+            
+            }
+        }
+    }
+}
