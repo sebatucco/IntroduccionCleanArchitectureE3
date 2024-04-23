53932ee7b4ef589785797cabaa8a16304194bd89
sebatucco <c.sebastian.ojeda@gmail.com>
2024-04-23 16:34:43 -0300

    Coonection factory

---
+++ IntroduccionCleanArchitectureE3.Application/Alquileres/GetAlquiler/GetAlquilerQuery.cs
@@ -0,0 +1,12 @@
+﻿using IntroduccionCleanArchitectureE3.Application.Abstractions.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionCleanArchitectureE3.Application.Alquileres.GetAlquiler
+{
+    public sealed record GetAlquilerQuery(Guid AlquilerId):IQuery<AlquilerResponse>;
+  
+}
---
+++ IntroduccionCleanArchitectureE3.Application/Alquileres/GetAlquiler/GetAlquilerQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using IntroduccionCleanArchitectureE3.Application.Abstractions.Data;
+using IntroduccionCleanArchitectureE3.Application.Abstractions.Messaging;
+using IntroduccionCleanArchitectureE3.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionCleanArchitectureE3.Application.Alquileres.GetAlquiler
+{
+    internal sealed class GetAlquilerQueryHandler : IQueryHandler<GetAlquilerQuery, AlquilerResponse>
+    {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public GetAlquilerQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public async Task<Result<AlquilerResponse>> Handle(GetAlquilerQuery request, CancellationToken cancellationToken)
+        {
+            using var connection = _sqlConnectionFactory.CreateConnection();
+
+            var alquiler = await connection.QueryFirstOrDefaultAsync<AlquilerResponse>(
+                sql,
+                new
+                {
+                    request.AlquilerId
+                }
+                );
+            return alquiler;
+        }
+    }
+}
