20721908ad40c1d7ef541c3061bc27874da16c78
sebatucco <c.sebastian.ojeda@gmail.com>
2024-04-25 12:34:31 -0300

    Consulta Dupper

--- IntroduccionCleanArchitectureE3.Application/Alquileres/GetAlquiler/GetAlquilerQueryHandler.cs
+++ IntroduccionCleanArchitectureE3.Application/Alquileres/GetAlquiler/GetAlquilerQueryHandler.cs
@@ -22,7 +22,25 @@ namespace IntroduccionCleanArchitectureE3.Application.Alquileres.GetAlquiler
         public async Task<Result<AlquilerResponse>> Handle(GetAlquilerQuery request, CancellationToken cancellationToken)
         {
             using var connection = _sqlConnectionFactory.CreateConnection();
-
+            var sql = """ 
+                SELECT
+                    id As Id,
+                    vehiculo_id AS VehiculoId,
+                    user_id AS UserId,
+                    status AS Status,
+                    precio_por_periodo AS PrecioAlquiler,
+                    precio_por_periodo_tipo_moneda AS TipoMonedaAlquiler,
+                    precio_mantenimiento AS PrecioMayrnimiento,
+                    precio_mantenimiento_tipo_moneda AS TipoMonedaMantenimiento,
+                    precio_accesorios AS AccesoriosPrecio,
+                    precio_accesorios_tipo_moneda AS TipoMonedaAccesorio,
+                    precio_total AS PrecioTotal,
+                    Precio_total_tipo_moneda AS PrecioTotalTipoMoneda,
+                    duracion_inicio AS DuracionInicio,
+                    duracion_final AS DuracionFinal,
+                    fecha_creacion AS FechaCreacion
+                FROM alquileres WHERE id = @AlquilerId
+            """;
             var alquiler = await connection.QueryFirstOrDefaultAsync<AlquilerResponse>(
                 sql,
                 new
