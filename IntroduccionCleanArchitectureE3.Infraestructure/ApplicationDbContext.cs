03f5c17f8714fae792d3a0160a3343baa51f783d
sebatucco <c.sebastian.ojeda@gmail.com>
2024-05-07 14:48:27 -0300

    creacion del context

---
+++ IntroduccionCleanArchitectureE3.Infraestructure/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using IntroduccionCleanArchitectureE3.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionCleanArchitectureE3.Infraestructure
+{
+    public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { 
+        }
+    }
+}
